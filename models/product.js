efc44841e0eedb224c64277666f1d4d671cc1a39
harshitaM29 <harshita.mundhe29@gmail.com>
2023-08-06 18:07:27 +0530

    product details page

--- models/product.js
+++ models/product.js
@@ -1,40 +1,48 @@
-let products = [];
 const fs = require('fs');
 const path = require('path');
+
 const p = path.join(
-    path.dirname(process.mainModule.filename),
-    'data',
-    'products.json'
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
 );
-const commonFunction = (cb) => {
-   
-    fs.readFile(p,(err,data) => {
-        if(err) {
-          return cb([])
-        } else {
-       cb(JSON.parse(data));
-        }
-    })
-}
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
-    constructor(title) {
-        this.title = title;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    }
-  
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    save() {
-     commonFunction(products => {
-        products.push(this);
-            fs.writeFile(p,JSON.stringify(products),(err) => {
-                console.log(err)
-            });
-     });
-      
-            
-          }
-    static fetchAll(cb) {
-     commonFunction(cb);
-       
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id,cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    })
+  }
+};
