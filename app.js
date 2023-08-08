f4a68fd5c342e501547f5ac867c3775ac2cb2ccb
harshitaM29 <harshita.mundhe29@gmail.com>
2023-08-08 11:11:44 +0530

    Used sequlize

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products').then().catch();
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,4 +25,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+sequelize.sync().then(res => {
+    
 app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+})
+
--- util/database.js
+++ util/database.js
@@ -1,10 +1,7 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'mysql@123'
+const sequelize = new Sequelize('node-complete','root','mysql@123',{
+    dialect:'mysql', host:'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
