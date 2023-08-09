a9b70f284b42008453512139f9e896d4c1b4989f
harshitaM29 <harshita.mundhe29@gmail.com>
2023-08-09 17:49:56 +0530

    cart add,delete functioanlity

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const errorController = require('./controllers/error');
 
 const sequelize = require('./util/database');
 const Product = require('./models/product');
-const User = require('./models/user')
+const User = require('./models/user');
+
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -36,6 +39,11 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints:true, onDelete:'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through:CartItem});
+Product.belongsToMany(Cart, {through:CartItem});
+
 sequelize.sync().then(res => {
     return User.findByPk(1);
 
@@ -47,7 +55,10 @@ sequelize.sync().then(res => {
     return user
 })
 .then(user => {
-  
+    return user.createCart();
+   
+})
+.then(cart => {
     app.listen(3000);
 })
 .catch(err => {
